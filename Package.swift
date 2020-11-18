d6f67ab25746fd555fe90b31b1f2e84374f59bff
Mike Robinson <mike.robinson@slicelife.com>
2020-11-18 17:41:11 +0000

    Adds Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "Japx",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "Japx",
+            targets: ["Japx"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Moya/Moya", from: "14.0.0"),
+        .package(name: "RxSwift", url: "https://github.com/ReactiveX/RxSwift", from: "5.1.0"),
+    ],
+    targets: [
+        .target(
+            name: "Japx",
+            dependencies: ["Moya",
+                           "RxSwift"],
+            path: "Japx"
+        ),
+    ]
+)
